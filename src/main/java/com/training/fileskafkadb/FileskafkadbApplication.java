159dac9808117ca33c6055db5c3d662530b4678a
Paras Tripathi <parastripathilearner@gmail.com>
2020-01-13 18:18:42 +0530

    Initial commit

---
+++ src/main/java/com/training/fileskafkadb/FileskafkadbApplication.java
@@ -0,0 +1,13 @@
+package com.training.fileskafkadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileskafkadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileskafkadbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/fileskafkadb/FileskafkadbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.fileskafkadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileskafkadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
