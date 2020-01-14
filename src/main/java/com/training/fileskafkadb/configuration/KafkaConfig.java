1c67704d731879abc72ce4a2125404914b78dc59
Jarvis25899 <35518754+Jarvis25899@users.noreply.github.com>
2020-01-14 23:21:27 +0530

    done autowired instances

--- src/main/java/com/training/fileskafkadb/configuration/KafkaConfig.java
+++ src/main/java/com/training/fileskafkadb/configuration/KafkaConfig.java
@@ -1,25 +1,44 @@
-package com.training.fileskafkadb.utilities.kafkaconfig;
+package com.training.fileskafkadb.configuration;
 
 import com.training.fileskafkadb.entity.Employee;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication(exclude = KafkaAutoConfiguration.class)
 @EnableKafka
 @Configuration
-public class KafkaListenerConfig {
+public class KafkaConfig {
+
+    @Bean
+    public ProducerFactory<String,Employee> producerFactory(){
+        Map<String,Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<String, Employee>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String,Employee> kafkaTemplate(){
+        return new KafkaTemplate<String,Employee>(producerFactory());
+    }
+
     @Bean
     public ConsumerFactory<String,Employee> employeeConsumerFactory(){
         Map<String,Object> config = new HashMap<>();
